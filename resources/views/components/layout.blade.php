81b467e0d9a431210839c47a3cab4776ff5a8676
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-04-17 21:39:06 +0900

    css fixed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,15 +9,13 @@
     <title>{{$title}}</title>
     @vite('resources/css/app.css')
     @vite('resources/js/index.js')
-    <script src="{{asset('js/scripts.js')}}"></script>
-    <script src="{{asset('css/app.css')}}"></script>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite('resources/js/app.js')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body class="overflow-hidden h-full">
 {{$slot}}
 
-<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+{{--<script src="../path/to/flowbite/dist/flowbite.min.js"></script>--}}
 </body>
 
 </html>
